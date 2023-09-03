6270c7877a3a5f80fcb0b0361316560f84a99dc7
anujrawat22 <anuj22rawat20@gmail.com>
2023-09-04 00:06:43 +0530

    first commit

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def json_form():
-    if request.method == 'POST':
-        try:
-            submitted_json = request.form['json_data']
-            return jsonify({'message': 'JSON submitted successfully', 'data': submitted_json})
-        except:
-            return jsonify({'error': 'Invalid JSON data'})
-
+@app.route('/')
+def index():
     return render_template('index.html')
+
+@app.route('/submit-json', methods=['POST'])
+def submit_json():
+    try:
+        data = request.get_json()
+        submitted_json = data.get('json_data', '')
+        return jsonify({'message': 'JSON submitted successfully', 'data': submitted_json})
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Invalid JSON data'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
